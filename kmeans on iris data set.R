c648066ee99129708745d69a4a107b52c5f43af5
Jeena Julius <108743003+jeenajulius@users.noreply.github.com>
2024-08-07 10:47:25 +0530

    Add files via upload

---
+++ kmeans on iris data set.R
@@ -0,0 +1,35 @@
+
+data("iris")
+head(iris)
+
+#remove species column
+library(dplyr)
+data<-select(iris,c(1:4))
+head(data)
+
+df<-data
+#remove rows with missing values
+df<-na.omit(df)
+
+install.packages("factoextra")
+library(factoextra)
+
+
+#Elbow test: to find optimal no of clusters
+
+fviz_nbclust(df,kmeans,method="wss")
+
+#implement kmeans using kmeans function
+set.seed(123)
+#optimal no of cluster is 2
+km<-kmeans(df,centers = 2,nstart=25)
+
+km
+km$cluster
+km$centers
+
+#view results
+fviz_cluster(km,data=df)
+
+library(ggfortify)
+autoplot(km,df,frame=TRUE)
